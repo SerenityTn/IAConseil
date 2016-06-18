40da0ab1adc11d441681fee29041a44206aeb878
Khaireddine <serenity1994@hotmail.fr>
2016-06-18 20:29:10 +0100

    affaires added

--- database/seeds/QuestionsTableSeeder.php
+++ database/seeds/QuestionsTableSeeder.php
@@ -13,23 +13,29 @@ class QuestionsTableSeeder extends Seeder{
     public function run(){
     	mb_regex_encoding('UTF-8');
     	$user = User::find(1);
-    	for($k = 1; $k <= 1; $k++){
-    		$dom = new DOMDocument();
-	    	$dom->loadHTML('<?xml encoding="utf-8" ?>'.file_get_contents(database_path()."/QR/affaires".$k.".html"));
-	    	$values = preg_split("[<h3>|</h3>]", $dom->saveHTML($dom->documentElement));
-	    	for($i = 0; $i < count($values)-1; $i+=2){
+    	for($k = 1; $k <= 4; $k++){
+            $dom = new DOMDocument();
+            $dom->loadHTML('<?xml encoding="utf-8" ?>'.file_get_contents(database_path()."/QR/affaires".$k.".html"));
+            $divs = $dom->getElementsByTagName('div');
+            echo $k . '\n';
+            foreach ($divs as $div) {
+                echo $div->nodeValue . "\n\n";
+                $h3 = $div->getElementsByTagName('h3')->item(0);
+                $question = new Question([
+                    "content" => $h3->nodeValue,
+                    "key_content" => $h3->nodeValue,
+                    "is_ia" => 1
+                ]);
 
-          $question = new Question([
-	    				"content" => html_entity_decode(trim(strip_tags($values[$i]))),
-	    				"key_content" => html_entity_decode(trim(strip_tags($values[$i]))),
-	    				"is_ia" => 1
-	    		]);
-
-				  $user->questions()->save($question);
-	    		$response = new Response;
-	    		$response->text = html_entity_decode(trim(strip_tags($values[$i+1])));
-	    		$question->responses()->save($response);
-	    	}
-    	}
+                if($h3 != null){
+                    $div->removeChild($h3);
+                    $user->questions()->save($question);
+                    $h3 = $div->getElementsByTagName('h3')->item(0);
+                    $response = new Response;
+	    		    $response->text = $div->nodeValue;
+                    $question->responses()->save($response);
+                }
+            }
+        }
     }
 }
